369b0603d84be4df70b7202a154bf19f8c383cf4
jagmeet singh <jagmeet.alagh@gmail.com>
2020-05-14 00:15:01 +0530

    prepared for deployment

--- index.js
+++ index.js
@@ -24,13 +24,11 @@ app.use( (error,req,res,next) => {
     return res.status(error.statusCode).json(error);
 });
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "client/build")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    });
-}
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 mongoose
     .connect(config.MONGO_URL)
