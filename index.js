e532d91d69a833f2b547c5d6bd693773598fa65a
jagmeet singh <jagmeet.alagh@gmail.com>
2020-05-13 23:41:16 +0530

    prepared for deployment

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const authRoutes = require('./routes/authRoutes');
 const jobRoutes = require('./routes/jobRoutes');
@@ -23,11 +24,13 @@ app.use( (error,req,res,next) => {
     return res.status(error.statusCode).json(error);
 });
 
-app.use(express.static(path.join(__dirname, "client", "build")))
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 mongoose
     .connect(config.MONGO_URL)
